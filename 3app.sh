0603da8388b02956932f808478384eeb8a9378c1
Neobee <88863739+iNeobee@users.noreply.github.com>
2021-11-01 21:35:30 +0700

    Update 3app.sh

--- 3app.sh
+++ 3app.sh
@@ -7,6 +7,8 @@ git clone https://aur.archlinux.org/vim-git.git
 cd aura-bin
 makepkg -si
 
+cd
+
 sudo pacman -S --needed polkit lxsession xorg-{server,xinit,xrandr,xbacklight,xprop,xsetroot,mkfontdir} xclip xf86-{input-libinput,video-intel} nvidia{,-utils} adobe-source-code-pro-fonts ttf-{cascadia-code,fira-code,jetbrains-mono} && sudo aura -Ax ttf-mononoki && sudo aura -Ax nerd-fonts-mononoki && sudo pacman -S --needed ttf-{baekmuk,hanazono} otf-ipafont adobe-source-han-{sans-{kr,jp},serif-{kr,jp}}-fonts && sudo pacman -S --needed rsync lsof cmake go lua mono npm ruby rust jre{8,11,}-openjdk{,-headless} jdk{8,11,}-openjdk
 
 sudo fc-cache -fv
@@ -15,6 +17,8 @@ cd my/downloads/clone/vim-git/
 sed -i '46s/disable/enable/' PKGBUILD
 sudo pacman -Rns vim
 makepkg -si
+
+cd
 
 sudo pacman -S --needed xmonad{,-contrib} xmobar python-pillow kitty{,-terminfo} && sudo aura -Ax google-chrome && sudo aura -Ax spacefm && sudo pacman -S --needed nitrogen picom dmenu rofi scrot && sudo aura -Ax i3lock-color && sudo pacman -S --needed lxappearance qt5ct arc-gtk-theme kvantum-qt5 papirus-icon-theme imagemagick sxiv mpv mpd ncmpcpp ffmpeg tar unzip zip && sudo aura -Ax rar && sudo pacman -S --needed file-roller udisks2 udevil ntfs-3g gvfs libmtp mtpfs gvfs-mtp && sudo aura -Ax jmtpfs && sudo pacman -S --needed libreoffice-fresh gimp inkscape obs-studio && sudo aura -Ax olive && sudo pacman -S --needed zathura{,-cb,-djvu,-pdf-mupdf,-ps}
 
