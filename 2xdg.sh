a2fbe09dd4bd73b44c714f4dbac57673f49c2cd7
Neobee <88863739+iNeobee@users.noreply.github.com>
2021-11-01 21:00:39 +0700

    Update 2xdg.sh

--- 2xdg.sh
+++ 2xdg.sh
@@ -1,17 +1,13 @@
 #!/bin/bash
 
-sudo ln -sf /etc/runit/sv/ufw /run/runit/service/ufw
-sudo ln -sf /etc/runit/sv/NetworkManager /run/runit/service/NetworkManager
-
-sudo ufw enable
-sudo ufw status verbose
-
+echo "=====:=====:===== Make XDG Base Dirs & Move Some Items =====:=====:====="
 mkdir -p ~/{.cache/{xmonad,thumbnails/{fail,large,normal,x-large,xx-large}},.config/{android,bash,java,xmonad,xmobar,zsh},.local/{share/xmonad,state/bash}}
 mv .bash* .config/bash/
 mv .config/bash/.bash_logout .config/bash/bash_logout
 mv .config/bash/.bash_profile .config/bash/bash_profile
 mv .config/bash/.bashrc .config/bash/bashrc
 
+echo "=====:=====:===== Update XDG User Dirs & Clone My Config =====:=====:====="
 xdg-user-dirs-update
 mkdir my/downloads/clone
 cd my/downloads/clone
@@ -20,4 +16,4 @@ cd
 cp -r my/downloads/clone/config/.config/{bash,i3lock-color,kitty,npm,picom,rofi,vim,wgetrc,X11,,xmobar,xmonad,zathura} .config/
 sudo cp my/downloads/clone/config/etc/profile.d/* /etc/profile.d/
 sudo cp my/downloads/clone/config/etc/bash/bashrc.d/second.bashrc /etc/bash/bashrc.d/
-echo "Pls Relog :)"
+echo "=====:=====:===== Pls Relog :) =====:=====:====="
