7f29932d37f47038f4567543aa7c7ce93a670167
Neobee <88863739+iNeobee@users.noreply.github.com>
2021-11-01 20:32:45 +0700

    Update 1base.sh

--- 1base.sh
+++ 1base.sh
@@ -3,6 +3,7 @@
 ## My Artix Linux Install Script
 #
 # Swapfile
+echo "Make a Swapfile"
 dd if=/dev/zero of=/swap/swapfile bs=1G count=2 status=progress
 chmod 600 /swap/swapfile
 mkswap /swap/swapfile
@@ -12,15 +13,18 @@ echo "# /swap/swapfile" >> /etc/fstab
 echo "/swap/swapfile    none    swap    defaults    0 0" >> /etc/fstab
 
 # Swappiness
+echo "Configure Swappiness"
 mkdir /etc/sysctl.d
 touch /etc/sysctl.d/99-swappiness.conf
 echo "vm.swappiness=10" >> /etc/sysctl.d/99-swappiness.conf
 
 # Timezone
+echo "Confiure Timezone"
 ln -sf /usr/share/zoneinfo/Asia/Jakarta /etc/localtime
 hwclock --systohc
 
 # Locale
+echo "Configure Locale"
 sed -i '177s/.//' /etc/locale.gen # en_US: change line 177 first char with nothing :)
 sed -i '289s/.//' /etc/locale.gen # id_ID
 sed -i '302s/.//' /etc/locale.gen # ja_JP
@@ -29,15 +33,18 @@ locale-gen
 echo "LANG=en_US.UTF-8" >> /etc/locale.conf
 
 # Hostname & hosts : Change the host name to what u want
+echo "Configuring Hostname & Hosts"
 echo "urHostName" >> /etc/hostname
 echo "127.0.0.1 localhost" >> /etc/hosts
 echo "::1       localhost" >> /etc/hosts
 echo "127.0.1.1 urHostName.localdomain  urHostName" >> /etc/hosts
 
 # Artix Arch Linux Support
+echo "Artix Arch Linux Support"
 pacman -S artix-archlinux-support pacman-contrib
 pacman-key --populate archlinux
 
+echo "Configuring /etc/pacman.conf"
 echo "" >> /etc/pacman.conf
 echo "# ARCH LINUX" >> /etc/pacman.conf
 echo "# [core]" >> /etc/pacman.conf
@@ -63,10 +70,12 @@ echo "Server = https://universe.artixlinux.org/$arch" >> /etc/pacman.conf
 pacman -Syu --needed grub efibootmgr networkmanager{,-runit} ufw{,-runit} alsa-utils pulseaudio{,-alsa} bash-completion xdg-{utils,user-dirs} bat htop lsd neofetch
 
 # Installing grub
+echo "Installing grub"
 grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB
 grub-mkconfig -o /boot/grub/grub.cfg
 
 # xdg user dirs
+echo "Configuring XDG User Dirs"
 sed -i 's/Desktop/desktop/' /etc/xdg/user-dirs.defaults
 sed -i 's/Downloads/my\/downloads/' /etc/xdg/user-dirs.defaults
 sed -i 's/Templates/my\/templates/' /etc/xdg/user-dirs.defaults
@@ -76,8 +85,14 @@ sed -i 's/Music/my\/music/' /etc/xdg/user-dirs.defaults
 sed -i 's/Pictures/my\/pictures/' /etc/xdg/user-dirs.defaults
 sed -i 's/Videos/my\/videos/' /etc/xdg/user-dirs.defaults
 
+echo "Change Your Root Password"
+passwd
+# Add a user
+echo "Adding a user"
+useradd -mG wheel user
+echo "Change Your User Password"
+passwd user
+EDITOR=vim visudo
+
 echo "=====:=====:===== Things Left =====:=====:====="
-echo "Change root passwd: passwd"
-echo "Add a user        : useradd -mG wheel ur_username"
-echo "Edit visudo       : EDITOR=vim visudo"
-echo "                    exit umount & reboot. DONE :)"
+echo "         exit umount & reboot. DONE :)         "
