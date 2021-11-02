442729be296e81168726edff516726cd2cfb8e5c
Neobee <88863739+iNeobee@users.noreply.github.com>
2021-11-02 17:04:41 +0700

    Update 2xdg.sh

--- 2xdg.sh
+++ 2xdg.sh
@@ -1,12 +1,5 @@
 #!/bin/bash
 
-echo "=====:=====:===== Enabling ufw & NetworkManager =====:=====:====="
-sudo ln -s /etc/runit/sv/ufw /run/runit/service/ufw
-sudo ln -s /etc/runit/sv/NetworkManager /run/runit/service/NetworkManager
-
-sudo ufw enable
-sudo ufw status verbose
-
 echo "=====:=====:===== Make XDG Base Dirs & Move Some Items =====:=====:====="
 mkdir -p ~/{.cache/{xmonad,thumbnails/{fail,large,normal,x-large,xx-large}},.config/{android,bash,java,xmonad,xmobar,zsh},.local/{share/xmonad,state/bash}}
 mv .bash* .config/bash/
@@ -27,7 +20,5 @@ sudo cp my/downloads/clone/config/etc/profile.d/* /etc/profile.d/
 sudo cp my/downloads/clone/config/etc/bash/bashrc.d/second.bashrc /etc/bash/bashrc.d/
 
 echo "=====:=====:===== Don't Forget to Source bashrc & bash_profile =====:=====:====="
-source .config/bash/bashrc
-source .config/bash/bash_profile
 
 echo "=====:=====:===== Pls Relog & remove .bash_history file :) =====:=====:====="
